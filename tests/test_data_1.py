257726c5d3db5f8e58949dc3b277774021e66479
chiri <chirizxc@proton.me>
2025-10-16 02:29:27 +0300

    fix

--- tests/test_data_1.py
+++ tests/test_data_1.py
@@ -7,5 +7,5 @@ import tomllib
 
 class TestSingleToml(unittest.TestCase):
     def test(self):
-        toml_str = Path("data.toml").read_text(encoding="utf-8")
+        toml_str = (Path(__file__).parent / "data.toml").read_text(encoding="utf-8")
         self.assertEqual(tomllib.loads(toml_str), toml_rs.loads(toml_str))
