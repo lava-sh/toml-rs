cc2c9238242f6a7cb966d339fe35ec6c2b8add64
chiri <chirizxc@proton.me>
2025-10-22 23:37:42 +0300

    add more tests (#30)
    * add more tests
    
    * fix ruff

--- tests/test_compatibility.py
+++ tests/test_compatibility.py
@@ -1,10 +1,12 @@
+import tomllib
 from pathlib import Path
 
 import pytest
 import toml_rs
-import tomllib
 
 from tests import tests_path
+from .burntsushi import convert, normalize
+from .test_data import VALID_PAIRS
 
 TOML = tests_path / "data" / "example.toml"
 
@@ -14,16 +16,28 @@ def test_toml():
     assert tomllib.loads(toml_str) == toml_rs.loads(toml_str)
 
 
-def test_toml_text_mode_typeerror():
-    with Path(TOML).open(encoding="utf-8") as f, pytest.raises(TypeError) as exc1:
-        tomllib.load(f)
-    tomllib_err = str(exc1.value)
-
-    with Path(TOML).open(encoding="utf-8") as f, pytest.raises(TypeError) as exc2:
-        toml_rs.load(f)
-    toml_rs_err = str(exc2.value)
-
+@pytest.mark.parametrize("lib", [tomllib, toml_rs])
+def test_text_mode_typeerror(lib):
     err_msg = "File must be opened in binary mode, e.g. use `open('foo.toml', 'rb')`"
-    assert err_msg in tomllib_err
-    assert err_msg in toml_rs_err
-    assert tomllib_err == toml_rs_err
+    with Path(TOML).open(encoding="utf-8") as f, pytest.raises(TypeError) as exc:
+        lib.load(f)
+    assert err_msg in str(exc.value)
+
+
+@pytest.mark.parametrize(
+    ("valid", "expected"),
+    VALID_PAIRS,
+    ids=[p[0].stem for p in VALID_PAIRS],
+)
+def test_tomllib_vs_tomlrs(valid, expected):
+    toml_str = valid.read_bytes().decode("utf-8")
+    try:
+        toml_str.encode("ascii")
+    except UnicodeEncodeError:
+        pytest.skip(f"Skipping Unicode content test: {valid.name}")
+    tomllib_ = normalize(convert(tomllib.loads(toml_str)))
+    toml_rs_ = normalize(convert(toml_rs.loads(toml_str)))
+
+    assert tomllib_ == toml_rs_, (
+        f"Mismatch between tomllib and toml_rs for {valid.name}"
+    )
