970d2de843d4d3ac931a448cfa9017f6c572ebe5
chiri <chirizxc@proton.me>
2025-11-22 00:06:24 +0300

    update

--- src/lib.rs
+++ src/lib.rs
@@ -20,7 +20,7 @@ static GLOBAL: mimalloc::MiMalloc = mimalloc::MiMalloc;
 
 #[cfg(feature = "snmalloc")]
 #[global_allocator]
-static ALLOC: snmalloc_rs::SnMalloc = snmalloc_rs::SnMalloc;
+static GLOBAL: snmalloc_rs::SnMalloc = snmalloc_rs::SnMalloc;
 
 import_exception!(toml_rs, TOMLDecodeError);
 import_exception!(toml_rs, TOMLEncodeError);
