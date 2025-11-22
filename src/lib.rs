c3bebfe67139d51bc84ff29bc93022a4641afeac
chiri <chirizxc@proton.me>
2025-11-22 13:48:34 +0300

    update

--- src/lib.rs
+++ src/lib.rs
@@ -18,6 +18,10 @@ use crate::{
 #[global_allocator]
 static GLOBAL: mimalloc::MiMalloc = mimalloc::MiMalloc;
 
+#[cfg(feature = "snmalloc")]
+#[global_allocator]
+static GLOBAL: snmalloc_rs::SnMalloc = snmalloc_rs::SnMalloc;
+
 import_exception!(toml_rs, TOMLDecodeError);
 import_exception!(toml_rs, TOMLEncodeError);
 
