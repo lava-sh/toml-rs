8002caa889dfa29ca81fd9ca8272f07c1caf8133
chiri <chirizxc@proton.me>
2026-03-21 16:29:49 +0300

    fix typos

--- noxfile.py
+++ noxfile.py
@@ -11,7 +11,7 @@ nox.options.reuse_existing_virtualenvs = True
 # tomli == 2.4.0 (Version 2.4.0 and later are compatible with TOML v1.1.0)
 @nox.session
 @nox.parametrize("tomli_version", ["2.3.0", "2.4.0"])
-def test_compatibillity(session: nox.Session, tomli_version: str) -> None:
+def test_compatibility(session: nox.Session, tomli_version: str) -> None:
     session.install(f"tomli=={tomli_version}")
     session.install("--group", "pytest")
     session.run("pytest")
