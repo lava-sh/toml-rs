4a803c67575e31f6d57a0ecc2c96221b0ba9a377
chiri <chirizxc@proton.me>
2026-04-06 18:06:15 +0300

    0.3.9

--- py-src/toml_rs/__init__.py
+++ py-src/toml_rs/__init__.py
@@ -10,11 +10,14 @@ __all__ = (
     "loads",
 )
 
+from _toml_rs import (  # noqa: PLC2701
+    _VERSION as __version__,  # noqa: N811
+)
+
 from ._lib import (
     TOMLDecodeError,
     TOMLDocument,
     TOMLEncodeError,
-    __version__,
     dump,
     dumps,
     load,
