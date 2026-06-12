167fd1ea14f9d4aab6b6ae20c8961c82a462b169
chiri <chirizxc@proton.me>
2026-06-12 12:59:32 +0300

    add support python 3.15, drop 3.13t (#208)

--- noxfile.py
+++ noxfile.py
@@ -6,13 +6,13 @@ nox.options.default_venv_backend = "uv"
 nox.options.reuse_existing_virtualenvs = True
 
 
-# https://pypi.org/project/tomli/2.3.0/
+# https://pypi.org/project/tomli/2.3.1
 # It is fully compatible with TOML v1.0.0
 #
-# https://pypi.org/project/tomli/2.4.0/
+# https://pypi.org/project/tomli/2.4.1
 # Version 2.4.0 and later are compatible with TOML v1.1.0
 @nox.session
-@nox.parametrize("tomli_version", ["2.3.0", "2.4.0"])
+@nox.parametrize("tomli_version", ["2.3.1", "2.4.1"])
 def test_compatibility(session: nox.Session, tomli_version: str) -> None:
     session.install(f"tomli=={tomli_version}")
     session.install("--group", "nox")
