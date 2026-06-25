ee2c1e42cbba9239b30738825cfbebe52bc36a56
chiri <chirizxc@proton.me>
2026-06-25 19:14:37 +0300

    try fix ty

--- noxfile.py
+++ noxfile.py
@@ -6,17 +6,10 @@ nox.options.default_venv_backend = "uv"
 nox.options.reuse_existing_virtualenvs = True
 
 
-# https://pypi.org/project/tomli/2.3.1
-# It is fully compatible with TOML v1.0.0
-#
-# https://pypi.org/project/tomli/2.4.1
-# Version 2.4.1 and later are compatible with TOML v1.1.0
 @nox.session
-@nox.parametrize("tomli_version", ["2.3.1", "2.4.1"])
-def test_compatibility(session: nox.Session, tomli_version: str) -> None:
-    session.install(f"tomli=={tomli_version}")
-    session.install("--group", "nox")
-
+@nox.parametrize("group_name", ["tomli_1", "tomli_1_1"])
+def test_compatibility(session: nox.Session, group_name: str) -> None:
+    session.install("--group", "nox", "--group", f"{group_name}")
     session.install(glob.glob("wheel/*.whl")[0])
 
     session.run("pytest", "tests/")
